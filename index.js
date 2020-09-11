2deb32cb6712839cb428ce00b3440c6ced5ccf3b
Robin Ploetzwich <r.ploetzwich@iks-gmbh.com>
2020-09-11 09:10:05 +0200

    initial upload

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+foo = chain('h')
+bar = foo('e')('l')('l')('o')
+foo.toString()            ==>  "h"
+bar.toString()             ==>  "hello"
+bar.ancestor.toString()     ==> "hell"
+ */
+
+function chain(value) {
+    // We are going to return a function, with a custom 'this' reference.
+    // We do this by invoking 'call' on our base method with a new object referencing
+    // the current object as the ancestor of the new function call.
+    const next = (nextValue) => chain.call({
+        ancestor: this,
+        // We also define this new objects toString function. This recursively get's the value
+        // of it's ancestors until we reach the first function call.
+        toString: () => this.ancestor ? this.toString() + value : value
+    }, nextValue);
+    // The toString method of the returned function also needs to be updated.
+    next.toString = () => this.ancestor ? this.toString() + value : value;
+    // At last we add the ancestor property to the retuned function, so we can reference it.
+    next.ancestor = this;
+
+    return next;
+}
+
+
+console.log(chain('h')('e').toString());
